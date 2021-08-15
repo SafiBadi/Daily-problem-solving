208b87efaa35bf3fbde2d3b8b1d2d8a7d4426d22
Safi <48173372+SafiBadi@users.noreply.github.com>
2021-08-15 22:48:02 +0530

    print distinct characters of a string

---
+++ distinct-characters-of-a-string.py
@@ -0,0 +1,28 @@
+# Python3 program to print distinct
+# characters of a string.
+NO_OF_CHARS = 256
+ 
+# Print duplicates present in the
+# passed string
+def printDistinct(str):
+ 
+    # Create an array of size 256 and
+    # count of every character in it
+    count = [0] * NO_OF_CHARS
+ 
+    # Count array with frequency of
+    # characters
+    for i in range (len(str)):
+        if(str[i] != ''):
+            count[ord(str[i])] += 1
+ 
+    # Print characters having count = 1
+    for i in range(len(str)):
+        if (count[ord(str[i])] == 1):
+            print (str[i], end = "")
+    print('\n',count[32])
+ 
+# Driver Code
+
+str = "Hello world"
+printDistinct(str)
